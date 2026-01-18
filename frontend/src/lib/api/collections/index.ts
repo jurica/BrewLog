6d31edf70eee27724d3be335784ebbcbd24dcfc5
Jurica Bacurin <jurica@bacurin.de>
2026-01-18 15:42:38 +0100

    cleanup

--- frontend/src/lib/api/collections/index.ts
+++ frontend/src/lib/api/collections/index.ts
@@ -3,12 +3,3 @@ export * from "./bags";
 export * from "./roasters";
 export * from "./cups";
 export * from "./users";
-
-// import { Bag } from "./bags";
-// import { Bean } from "./beans";
-// import { Cups } from "./cups";
-// import { Roaster } from "./roasters";
-
-//
-// export type RecordTypes = Bean.Record | Bag.Record | Roaster.Record | Cups.Record;
-// type t = keyof Bean.Record | keyof Bag.Record;
